89e9147e9ca16d468adef4ce0c43f0d105023097
notbeforecoffee <83381139+notbeforecoffee@users.noreply.github.com>
2022-07-16 00:15:27 -0400

    initial commit for Draft-js text editor.  Some functions still need to be removed

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+import DOMPurify from 'dompurify';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import './App.css';
+
+
+const App = () => {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
+  const  [convertedContent, setConvertedContent] = useState(null);
+  const handleEditorChange = (state) => {
+    setEditorState(state);
+    convertContentToHTML();
+  }
+  const convertContentToHTML = () => {
+    let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(currentContentAsHTML);
+  }
+  const createMarkup = (html) => {
+    return  {
+      __html: DOMPurify.sanitize(html)
+    }
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        Rich Text Editor Example
+      </header>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={handleEditorChange}
+        wrapperClassName="wrapper-class"
+        editorClassName="editor-class"
+        toolbarClassName="toolbar-class"
+      />
+
+      {/* this div provides a preview of the text box contents.  it runs a few seconds behind, and scared the shit out of me the first time, because I thought I broke something :p */}
+      <div className="preview" dangerouslySetInnerHTML={createMarkup(convertedContent)}></div>
+    </div>
+  )
+}
+export default App;
