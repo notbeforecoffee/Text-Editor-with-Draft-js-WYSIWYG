b981746cc90e8a142f037e1b5b7cec4e376c07f2
notbeforecoffee <83381139+notbeforecoffee@users.noreply.github.com>
2022-07-16 10:44:04 -0400

    new interface with streamlined toolbar

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import React, { useState } from 'react';
-import { EditorState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import { convertToHTML } from 'draft-convert';
-import DOMPurify from 'dompurify';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import './App.css';
-
+import React, { useState } from "react";
+import { EditorState } from "draft-js";
+import { Editor, Modifier } from "react-draft-wysiwyg";
+import { convertToHTML } from "draft-convert";
+import DOMPurify from "dompurify";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "./App.css";
 
 const App = () => {
-  const [editorState, setEditorState] = useState(
-    () => EditorState.createEmpty(),
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
   );
-  const  [convertedContent, setConvertedContent] = useState(null);
+  const [convertedContent, setConvertedContent] = useState(null);
   const handleEditorChange = (state) => {
     setEditorState(state);
     convertContentToHTML();
-  }
+  };
   const convertContentToHTML = () => {
     let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
     setConvertedContent(currentContentAsHTML);
-  }
+  };
   const createMarkup = (html) => {
-    return  {
-      __html: DOMPurify.sanitize(html)
-    }
-  }
+    return {
+      __html: DOMPurify.sanitize(html),
+    };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        Rich Text Editor Example
-      </header>
+      <header className="App-header">Notes</header>
       <Editor
         editorState={editorState}
         onEditorStateChange={handleEditorChange}
         wrapperClassName="wrapper-class"
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
+        toolbar={{
+          options: ["inline", "list", "textAlign", "history"],
+          //other available options:[ 'blockType', 'fontSize', 'embedded', 'emoji', image']
+        }}
       />
 
       {/* this div provides a preview of the text box contents.  it runs a few seconds behind, and scared the shit out of me the first time, because I thought I broke something :p */}
-      <div className="preview" dangerouslySetInnerHTML={createMarkup(convertedContent)}></div>
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={createMarkup(convertedContent)}
+      ></div>
     </div>
-  )
-}
+  );
+};
 export default App;
